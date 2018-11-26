c90cce4b028e6096d490701a1d4171414e943b24
JoanGE <joangarcia@iti.es>
2018-11-26 16:04:30 +0100

    simple bugfixes

--- siml/signal_analysis_utils.py
+++ siml/signal_analysis_utils.py
@@ -4,12 +4,14 @@ from scipy.signal import welch
 from scipy.fftpack import fft
 from scipy import signal
 
-from detect_peaks import detect_peaks
+from siml.detect_peaks import detect_peaks
+
 
 def get_values(y_values, T, N, f_s):
     y_values = y_values
     x_values = [(1/f_s) * kk for kk in range(0,len(y_values))]
     return x_values, y_values
+
 
 def get_fft_values(y_values, T, N, f_s):
     f_values = np.linspace(0.0, 1.0/(2.0*T), N//2)
@@ -17,19 +19,23 @@ def get_fft_values(y_values, T, N, f_s):
     fft_values = 2.0/N * np.abs(fft_values_[0:N//2])
     return f_values, fft_values
 
+
 def get_psd_values(y_values, T, N, f_s):
     f_values, psd_values = welch(y_values, fs=f_s)
     return f_values, psd_values
 
+
 def autocorr(x):
     result = np.correlate(x, x, mode='full')
     return result[len(result)//2:]
- 
+
+
 def get_autocorr_values(y_values, T, N, f_s):
     autocorr_values = autocorr(y_values)
     x_values = np.array([T * jj for jj in range(0, N)])
     return x_values, autocorr_values
-    
+
+
 def get_first_n_peaks(x,y,no_peaks=5):
     x_, y_ = list(x), list(y)
     if len(x_) >= no_peaks:
@@ -37,12 +43,14 @@ def get_first_n_peaks(x,y,no_peaks=5):
     else:
         missing_no_peaks = no_peaks-len(x_)
         return x_ + [0]*missing_no_peaks, y_ + [0]*missing_no_peaks
-    
+
+
 def get_features(x_values, y_values, mph):
     indices_peaks = detect_peaks(y_values, mph=mph)
     peaks_x, peaks_y = get_first_n_peaks(x_values[indices_peaks], y_values[indices_peaks])
     return peaks_x + peaks_y
- 
+
+
 def extract_features_labels(dataset, labels, T, N, f_s, denominator):
     percentile = 5
     list_of_features = []
